b5f0021156987eefebec02c11987e28892f51faf
Graham Norbury <gnorbury@bondcar.com>
2020-04-22 21:48:29 -0400

    Attached gnuplot to demodulator to observe symbol quality

--- python/fsk_demod.py
+++ python/fsk_demod.py
@@ -10,56 +10,61 @@ from gnuradio import eng_notation
 from gnuradio import filter
 from math import pi
 
+import gr_gnuplot
+
 class fsk_demod(gr.hier_block2):
-	def __init__(self, options):
-		gr.hier_block2.__init__(self, "fsk_demod",
+    def __init__(self, options):
+        gr.hier_block2.__init__(self, "fsk_demod",
                                 gr.io_signature(1, 1, gr.sizeof_gr_complex), # Input signature
                                 gr.io_signature(1, 1, gr.sizeof_char)) # Output signature
 
-		self._syms_per_sec = options.syms_per_sec # ditto
-		self._samples_per_second = options.samples_per_second
-		self._gain_mu = options.gain_mu # for the clock recovery block
-		self._mu = options.mu
-		self._omega_relative_limit = options.omega_relative_limit
+        self._syms_per_sec = options.syms_per_sec # ditto
+        self._samples_per_second = options.samples_per_second
+        self._gain_mu = options.gain_mu # for the clock recovery block
+        self._mu = options.mu
+        self._omega_relative_limit = options.omega_relative_limit
 
-		self._freqoffset = options.offset
+        self._freqoffset = options.offset
 
-		#first bring that input stream down to a manageable level, let's say 3 samples per bit.
-		self._clockrec_oversample = 3
+        #first bring that input stream down to a manageable level, let's say 3 samples per bit.
+        self._clockrec_oversample = 3
 
-		self._downsampletaps = filter.firdes.low_pass(1, self._samples_per_second, 10000, 1000, filter.firdes.WIN_HANN)
+        self._downsampletaps = filter.firdes.low_pass(1, self._samples_per_second, 10000, 1000, filter.firdes.WIN_HANN)
 
-		self._decim = int(self._samples_per_second / (self._syms_per_sec * self._clockrec_oversample))
+        self._decim = int(self._samples_per_second / (self._syms_per_sec * self._clockrec_oversample))
 
-		print "Demodulator decimation: %i" % (self._decim,)
-		self._downsample = filter.freq_xlating_fir_filter_ccf(self._decim, #decimation
-														  self._downsampletaps, #taps
-														  self._freqoffset, #freq offset
-														  self._samples_per_second) #sampling rate
+        print "Demodulator decimation: %i" % (self._decim,)
+        self._downsample = filter.freq_xlating_fir_filter_ccf(self._decim, #decimation
+                                                              self._downsampletaps, #taps
+                                                              self._freqoffset, #freq offset
+                                                              self._samples_per_second) #sampling rate
 
-		#using a pll to demod gets you a nice IIR LPF response for free
-		self._demod = analog.pll_freqdet_cf(2.0 / self._clockrec_oversample, #gain alpha, rad/samp
-										 2*pi/self._clockrec_oversample,  #max freq, rad/samp
-										-2*pi/self._clockrec_oversample)  #min freq, rad/samp
+        #using a pll to demod gets you a nice IIR LPF response for free
+        self._demod = analog.pll_freqdet_cf(2.0 / self._clockrec_oversample, #gain alpha, rad/samp
+                                            2*pi/self._clockrec_oversample,  #max freq, rad/samp
+                                            -2*pi/self._clockrec_oversample)  #min freq, rad/samp
 
-		self._sps = float(self._samples_per_second)/self._decim/self._syms_per_sec
+        self._sps = float(self._samples_per_second)/self._decim/self._syms_per_sec
 
-		#band edge filter FLL with a low bandwidth is very good
-		#at synchronizing to continuous FSK signals
-		self._carriertrack = digital.fll_band_edge_cc(self._sps,
-													  0.6, #rolloff factor
-													  64,  #taps
-													  1.0) #loop bandwidth
+        #band edge filter FLL with a low bandwidth is very good
+        #at synchronizing to continuous FSK signals
+        self._carriertrack = digital.fll_band_edge_cc(self._sps,
+                                                      0.6, #rolloff factor
+                                                      64,  #taps
+                                                      1.0) #loop bandwidth
 
-		print "Samples per symbol: %f" % (self._sps,)
-		self._softbits = digital.clock_recovery_mm_ff(self._sps,
-												 0.25*self._gain_mu*self._gain_mu, #gain omega, = mu/2 * mu_gain^2
-												 self._mu, #mu (decision threshold)
-												 self._gain_mu, #mu gain
-												 self._omega_relative_limit) #omega relative limit
+        print "Samples per symbol: %f" % (self._sps,)
+        self._softbits = digital.clock_recovery_mm_ff(self._sps,
+                                                      0.25*self._gain_mu*self._gain_mu, #gain omega, = mu/2 * mu_gain^2
+                                                      self._mu, #mu (decision threshold)
+                                                      self._gain_mu, #mu gain
+                                                      self._omega_relative_limit) #omega relative limit
 
-		self._subtract = blocks.sub_ff()
+        self._subtract = blocks.sub_ff()
 
-		self._slicer = digital.binary_slicer_fb()
+        self._slicer = digital.binary_slicer_fb()
 
-		self.connect(self, self._downsample, self._carriertrack, self._demod, self._softbits, self._slicer, self)
+        self.symbol_sink = gr_gnuplot.symbol_sink_f()
+
+        self.connect(self, self._downsample, self._carriertrack, self._demod, self._softbits, self._slicer, self)
+        self.connect(self._softbits, self.symbol_sink)
